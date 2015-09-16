c83a70c1e7c312e98889f763df139da1507500e4
p0dee <p0dee@p0dees-MacBook.local>
2015-09-16 16:25:27 +0900

    プロジェクト作成

---
+++ PDAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDAlertView
+//
+//  Created by Takeshi Tanaka on 9/16/15.
+//  Copyright © 2015 Takeshi Tanaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDAlertView
+//
+//  Created by Takeshi Tanaka on 9/16/15.
+//  Copyright © 2015 Takeshi Tanaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
